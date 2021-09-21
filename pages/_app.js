4a37e2a59fbf44089510d9066a0b0f36f6f4ab20
Royal6969 <sergiodiazcampos@gmail.com>
2021-09-21 23:39:18 +0200

    building header component

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
-// npx create-next-app -e with-tailwindcss google-docs
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css'; // npx create-next-app -e with-tailwindcss google-docs
+import '@material-tailwind/react/tailwind.css'; // npm i -E @material-tailwind/react
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* Material Icons Link */}
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
