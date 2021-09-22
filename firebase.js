24d321f296ee5400877860cf1c803ab124048921
Royal6969 <sergiodiazcampos@gmail.com>
2021-09-22 17:44:57 +0200

    implementing firebase v9 (probably we will have some issue and we will have to go back to v8)

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBKJm02UpoamWJAsfesCRgrsi2xaXwYroo",
+    authDomain: "docs-62b3d.firebaseapp.com",
+    projectId: "docs-62b3d",
+    storageBucket: "docs-62b3d.appspot.com",
+    messagingSenderId: "1088920908433",
+    appId: "1:1088920908433:web:acdd6f8d37b8ee12120530"
+};
+
+const app = !firebase.apps.length               // if there aren't any apps inizialited yet
+    ? firebase.initializeApp(firebaseConfig)    // we inizialite for first time
+    : firebase.app();                           // or we use one that we alredy got
+    
+const db = app.firestore();
+
+export { db };
